11399a202746e56dc724245959f5d98451989b35
Carmelo908 <carmelopepigauna@gmail.com>
2025-06-09 17:08:33 -0300

    Started repo

---
+++ agenda-contactos/Clases/Contacto.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 9/6/2025
+ * Time: 14:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Runtime.Serialization;
+
+namespace agenda_contactos.Clases
+{
+	/// <summary>
+	/// Description of Contacto.
+	/// </summary>
+	[DataContract]
+	public class Contacto
+	{
+		private string nombre;
+		private string email;
+		private string telefono;
+		private string movil;
+		private string domicilio;
+		private string notas;
+		
+		[DataMember]
+		public string Nombre {
+			get { return nombre; }
+			set { nombre = value; }
+		}
+		
+		[DataMember]
+		public string Email {
+			get { return email; }
+			set { email = value; }
+		}
+		
+		[DataMember]
+		public string Telefono {
+			get { return telefono; }
+			set { telefono = value; }
+		}
+
+		[DataMember]
+		public string Movil {
+			get { return movil; }
+			set { movil = value; }
+		}
+
+		[DataMember]
+		public string Domicilio {
+			get { return domicilio; }
+			set { domicilio = value; }
+		}
+
+		[DataMember]
+		public string Notas {
+			get { return notas; }
+			set { notas = value; }
+		}
+		
+		public Contacto()
+		{
+		}
+	}
+}
---
+++ agenda-contactos/MainForm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 9/6/2025
+ * Time: 14:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace agenda_contactos
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public Button botonAñadir;
+		public Button botonEliminar;
+		public Button botonModificar;
+		public DataGridView cuadriculaContactos;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			crearLayout();
+		}
+		
+		private void crearLayout()
+		{
+			FlowLayoutPanel panelBotones = new FlowLayoutPanel { AutoSize = true };
+			FlowLayoutPanel panelPrincipal = new FlowLayoutPanel { 
+				AutoSize = true, FlowDirection = FlowDirection.TopDown 
+			};
+			botonAñadir = crearBoton("Añadir", panelBotones);
+			botonEliminar = crearBoton("Eliminar", panelBotones);
+			botonModificar = crearBoton("Modificar", panelBotones);
+			panelPrincipal.Controls.Add(panelBotones);
+			panelPrincipal.Controls.Add(cuadriculaContactos);
+			Controls.Add(panelPrincipal);
+		}
+		
+		private Button crearBoton(string texto, Panel padre)
+		{
+			Button result = new Button { 
+				AutoSize = true,
+				Text = texto,		
+			};
+			padre.Controls.Add(result);
+			return result;
+		}
+	}
+}
